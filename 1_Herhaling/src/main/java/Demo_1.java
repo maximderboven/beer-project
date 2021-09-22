279dc868589dbd89229122c50a4b138104385b3c
Maxim Derboven <maxim.derboven@student.kdg.be>
2021-09-22 14:43:07 +0200

    Eerste module toegevoegd

---
+++ 1_Herhaling/src/main/java/Demo_1.java
@@ -0,0 +1,9 @@
+/**
+ * @author Maxim Derboven
+ * @version 1.0 22/09/2021 14:38
+ */
+public class Demo_1 {
+    public static void main(String[] args) {
+        System.out.println("Gradle working!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Groeiproject Programmeren 2 - Java
+* Naam: Maxim Derboven
+* Klasgroep: INF203A
+* Studentennummer: xxxx
+* Academiejaar: 2021-2022
